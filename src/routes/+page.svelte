04c586b90e6fb8b85418b1092d3624175e4a1722
Maksiks <maksiksking@gmail.com>
2025-08-15 19:46:35 +0300

    ifx: removed a unit test because too much work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,9 @@ import {enhance} from "$app/forms";
 </script>
 <main>
     <h1>Oh that's useful</h1>
+    <form method="POST" action="?/testApi" use:enhance>
+        <button type="submit">reeeeeeeeeeeeeeee</button>
+    </form>
     {#each data.nifties as nift (nift.id)}
     <div class="card">
         <img src={nift.favicon} alt="favicon">
@@ -13,9 +16,6 @@ import {enhance} from "$app/forms";
         <a href={nift.link}>🔗 link</a>
         <p>{nift.comment}</p>
         <img src="https://placehold.it/100x100.jpg" alt="placeholder">
-        <form method="POST" action="?/testApi" use:enhance>
-            <button type="submit">reeeeeeeeeeee</button>
-        </form>
     </div>
     {/each}
 </main>
