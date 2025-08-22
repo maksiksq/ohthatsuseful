d851eb14641200abaa19faa97e7b33e96c1df66a
Maksiks <maksiksking@gmail.com>
2025-08-22 13:54:19 +0300

    feat: made footer look a bit more fun

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,6 @@
 <script>
+    import {browser} from "$app/environment";
+
     const {fromContact = false} = $props();
 
     const socials = [
@@ -23,7 +25,25 @@
             link: 'https://ko-fi.com/maksiks',
         }
     ]
+
+    //
+
+    let footerBgX = $state(0);
+    const updateFooterBg = () => {
+        const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+        const currentScroll = window.scrollY;
+
+        const t = currentScroll / maxScroll;
+
+        const raw = -(currentScroll + window.innerHeight) / 28;
+
+        footerBgX = raw * (1 - t);
+    }
+
+    $inspect(footerBgX)
 </script>
+
+<svelte:window onscroll={updateFooterBg} />
 
 <footer>
     <div class="crank-seg">
@@ -43,6 +63,9 @@
                 <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
             </svg>
         </a>
+    </div>
+    <div class="footerBg">
+        <img style={`right: ${footerBgX}px;`} src="/img/footerbg.svg" role="presentation" alt="footer background">
     </div>
     <div class="text-seg">
         <!--        <a class="contact" href="/contact">Contact</a>-->
@@ -88,6 +111,7 @@
         display: flex;
         align-items: center;
         width: 100%;
+        position: relative;
 
         & .crank-seg {
             display: flex;
@@ -173,6 +197,17 @@
 
         }
 
+        & .footerBg {
+            & img {
+                position: absolute;
+                right: 0;
+                bottom: 0;
+                width: 30vw;
+
+                transition: right 0.6s linear(0, 0.012 0.9%, 0.05 2%, 0.411 9.2%, 0.517 11.8%, 0.611 14.6%, 0.694 17.7%, 0.765 21.1%, 0.824 24.8%, 0.872 28.9%, 0.91 33.4%, 0.939 38.4%, 0.977 50.9%, 0.994 68.4%, 1);
+            }
+        }
+
         & .text-seg {
             display: flex;
             justify-content: flex-end;
@@ -180,9 +215,6 @@
             padding-bottom: 0;
             width: 30%;
             font-size: 1.2rem;
-
-            background-image: url("/img/footerbg.svg");
-            background-size: cover;
 
             & .contact {
                 margin-top: 1rem;
@@ -199,7 +231,13 @@
                 padding: 2rem 2rem 0 2rem;
 
                 border-bottom: 2px solid #151515;
-                background-color: white;
+                border-top: 1px solid #151515;
+
+                /* From https://css.glass */
+                background: rgba(255, 255, 255, 0.2);
+                box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+                backdrop-filter: blur(5px);
+                -webkit-backdrop-filter: blur(5px);
             }
 
             & .social {
